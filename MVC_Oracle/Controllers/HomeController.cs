906808928111ae0c7f909b8ddcf9fa49273bb997
signsoluciones <reptors58@hotmail.com>
2019-04-09 17:23:01 -0500

    Agregar archivos de proyecto.

---
+++ MVC_Oracle/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using MVC_Oracle.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Oracle.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            try
+            {
+                using (var conexion = new EntitiesOracle())
+                {
+                    var _clientes = (from c in conexion.CLIENTES select c);
+                    return View(_clientes.ToList());
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            
+        }
+    }
+}
